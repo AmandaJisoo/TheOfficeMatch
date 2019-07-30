639f9bb5810fb82e51f346554ba308a12864e5a8
Amanda Park <wd1204@uw.edu>
2019-07-29 21:01:48 -0700

    css width added

--- script_new.js
+++ script_new.js
@@ -9,7 +9,7 @@ let rowSize = -1;// set default value as small
 let colSize = -1;
 let gameTimeLimit = -1;
 let timer = null;
-let gameStarted = false;
+let gameOnGoing = false;
 let cards = [
     '1.jpg', 
     '2.jpg', 
@@ -32,26 +32,28 @@ let cards = [
 //the card that has been chosen according to the input choice
 let chosenCards = [];
 //list of matched cards
-let matched = [];
-let clickedToMatch=[];
+let matched = new Set();
+console.log(matched.has(5));
+// let matched = [];
+let clickedToCheck=[];
 $(".game-board").hide();// doesn't show at first
 $(".start-btn").on("click", handleStartClick);//start press then level
 $(".size").on("click", sizeCallBack);
-$(".restart-btn").on("click", handleStartClick);
+$(".restart-btn").on("click", restart);
+
 
 //start the game
 function handleStartClick() {
-    gameStarted = true;
+    if (!gameOnGoing) {
+        //the user clicked restart button
+        gameOver();
+        gameOnGoing = true;
+        console.log("onGOing");
+    }
     $(".lost-statement").hide();
     $(".introduction").hide();
-    $(".remaining-time").hide();
     cards = _.shuffle(cards);
-    //when Ipush restart, te board should be hidden 
-    //size option should be displayed
-    $(".game-board").empty();//clean up the board
-    $(".game-board").hide();//hide it
     $(".start-btn").hide();
-    $(".restart-btn").hide();
     displaySizeOption();//disaply game size option and create gameboard
 }
 
@@ -69,15 +71,15 @@ function sizeAndTimeSet(event) {
         console.log(event.target.id);
         rowSize = SIZE_SMALL;
         colSize = SIZE_SMALL;
-        gameTimeLimit = 2;
+        gameTimeLimit = 60;
     } else if (event.target.id === "levelTwo") {
         rowSize = SIZE_MEDIUM;
         colSize = SIZE_MEDIUM;
-        gameTimeLimit = 4;
+        gameTimeLimit = 60;
     } else if (event.target.id === "levelThree") {
         rowSize =SIZE_BIG;
         colSize =SIZE_BIG;
-        gameTimeLimit = 6;
+        gameTimeLimit = 1000;
     }
 }
 
@@ -86,6 +88,7 @@ function sizeCallBack(event) {
     $(".remaining-time").text("0");
     gameBoadPrep(event);
     playGameHandler(event);
+    $(".personal-info").show();
     $(".size").hide();
     $(".restart-btn").show();
 }
@@ -107,34 +110,54 @@ function gameBoadPrep(event) {
     $(".game-board").css("grid-template-columns", "repeat(" + 4 + ", auto)");
     while (!jQuery.isEmptyObject(chosenCards)) {
         let chosenPic = chosenCards.pop();
-        //??? can I do this? like make them individual event
-        $(chosenPic).on("click", faceToBack(chosenPic));
-        //add on click listener to the card
-        //append both front and back to the same div
-        $(".game-board").append("<div class=card-face id=" + chosenPic + "><img src=img/" + chosenPic + " /></div>");
-        // $(".game-board").append("<div class=card-face id=" + chosenPic + "><img src=img/" + chosenPic + " /></div>");
-        console.log($('#' + chosenPic).val());
+        let card = $("<div class=card></div>");
+        card.append("<div class='card-face hidden' id=" + chosenPic + "><img src=img/" + chosenPic + " /></div>");
+        card.append("<div class='card-back' id=backImg><img src=img/backImg.jpg></div>");
+        $(".game-board").append(card);
+        //give listener to all cards
     } 
-   
-    // console.log(".game-board");
-    //********HERE FIGURE OUT HOW TO MAKE ALL VISIBLE TO BACK CARD */
+    $(".card").on("click", function(event) {
+        console.log(canBeFlip(event));
+        console.log( $(this).children('.card-face').id); // ehy undefined?
+        console.log($(this).children('.card-back')); 
+        $(this).children('.card-back').toggle("hide");   
+        $(this).children('.card-face').toggle("hide");  
+        if ($( this ).css( "transform" ) == 'none' ){
+            $(this).css("transform","360deg");
+        } else {
+            $(this).css("transform","" );
+        }
+    }); 
 }
 
-//things to do: how to flip to back card
-function faceToBack(card) {
-    console.log("Fliped to back image");
+//1. previously matched
+//2.only upto two cards is allowe to be open 
+//change parameter to the string event id?
+function canBeFlip(element) {
+    //cnage to includes name
+    return (matched.includes($(element).children('.card-face')) && clickedToCheck.length < 2);
 }
 
+function isMatch(event) {
+    if (clickedToCheck.includes(event)) {
+        matched.push(event);
+        //flip remove toggle class fuctionality
+    }
+}
+
+function isMisMathc(event) {
+    //then rotate back and
+}
 //must be length 2 to be called
 //maybe have a eay to make as an event?
 function cardOpen() {
-    if (clickedToMatch.length == 2) {
-        if (clickedToMatch[0] == clickedToMatch[1]) {
-            matched.push(clickedToMatch[0]);
+    if (clickedToCheck.length == 2) {
+        if (clickedToCheck[0] == clickedToCheck[1]) {
+            matched.push(clickedToCheck[0]);
             console.log("macthed");
         } else {
             //clear ou the currently attempted to match items
-            clickedToMatch = [];
+            clickedToCheck = [];
         }
     }
 }
@@ -145,31 +168,15 @@ function playGameHandler(event) {
     startCountDown();
     console.log("inside");
     $(".game-board").show(); //now show the game board
-
 }
 
-//card - card that user clicked
-// function flipCard(card) {
-//     if (canFlipCard(card)) {
-//         //remove the back and show front
-//         $(card).show();// show front side card
-//     }
-//     // if (cardToCheck()) {
-
-//     // }
-// }
-
-// function canFlipCard() {
-//     //first the card is not currently cliked card
-//     return !busy
-// }
 function startCountDown() {
     $(".remaining-time").show();
     $(".remaining-time").text(gameTimeLimit);
     //every one second 
-    let timer = setInterval(() => {
+    timer = setInterval(() => {
         gameTimeLimit--;
-        $(".remaining-time").html(gameTimeLimit);
+        $(".remaining-time").html("Timer:   " + gameTimeLimit);
         if(gameTimeLimit === 0) {
             gameOver(timer);
         }
@@ -200,17 +207,34 @@ function startCountDown() {
 //     //condition1: card must not be currently open
 //     return !gameStarted &&
 // }
+function restart() {
+    hideInfo();
+    gameOnGoing = true;
+    handleStartClick();
+    $(".restart-btn").hide();
+}
 
-function gameOver(timer) {
-    gameStarted = false;
+function hideInfo() {
     clearInterval(timer);
+    $(".game-board").empty();
+    $(".personal-info").hide();
     $(".game-board").hide();
     $(".remaining-time").hide();
+    $(".lost-statement").hide();
+    matched = [];//clear out the matched cardlist
+}
+
+function gameOver(timer) {
+    gameOnGoing = false;
+    clearInterval(timer);
+    hideInfo(timer);
     $(".lost-statement").show();
     $(".score").hide();
-    matched = [];//clear out the matched cardlist
     console.log("over");
 }
+
+
+
 
 
 
